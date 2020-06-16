f41917d396c23b8084041d6020e8c17d7def819c
Rodrigo Sommacal <rodrigosommacal@gmail.com>
2020-06-16 20:57:43 +0200

    challenge done

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,4 +1,20 @@
 class QuestionsController < ApplicationController
   def ask
   end
+
+  def answer
+    @question = params[:question]
+
+    @answer = coach_answer(@question) if @question
+  end
+
+  def coach_answer(your_message)
+    if your_message.downcase == "i'm going to work right now!"
+      'Good'
+    elsif your_message.include? '?'
+      'Silly question, get dressed and go to work!'
+    else
+      'I don\'t care, get dressed and go to work!'
+    end
+  end
 end
